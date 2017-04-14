1d3efd6ad3be7c929b61514f0351df783a499d59
SerhiiKostiuk <serhii.kostiuk@foreway.pro>
2017-04-14 17:50:43 +0300

    add album model

---
+++ VKPhotoViewerSwiftEdition/Source/Models/VKAlbumModel/VKAlbumModel.swift
@@ -0,0 +1,22 @@
+//
+//  VKAlbumModel.swift
+//  VKPhotoViewerSwiftEdition
+//
+//  Created by Serhii Kostiuk on 4/14/17.
+//  Copyright © 2017 Serhii Kostiuk. All rights reserved.
+//
+
+import Foundation
+
+class AlbumModel {
+    var albumId: Int = 0
+    var ownerId: Int = 0
+    var title: String = ""
+    var albumDescription: String = ""
+    var thumbUrl: String = ""
+    
+    
+    
+    
+    
+}
